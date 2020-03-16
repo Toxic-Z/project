f2c0ca0d86a81758dab866295649ce101beb6859
Toxic-Z <romakingforever@gmail.com>
2020-03-16 22:51:22 +0200

    update

--- drumncode/src/app/app.component.ts
+++ drumncode/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Observable } from 'rxjs';
+import { PowerPlant } from '../shared/interfaces/power-plant';
+import {ApiService} from '../shared/services/api.service';
+import {map, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'drumncode';
+export class AppComponent implements OnInit {
+  public powerPlants: Observable<PowerPlant[]>;
+  public activePp: number = null;
+  constructor(
+    private title: Title,
+    private apiService: ApiService
+  ) {
+   title.setTitle('Power Plants & Households');
+   this.powerPlants = this.apiService.fetchPowerPlants().pipe(
+     map((r: PowerPlant[]) => r),
+     tap((r: PowerPlant[]) => {
+       console.log(r);
+       // this.activePp = r[0].id;
+     })
+   );
+  }
+  ngOnInit(): void {
+
+  }
 }
--- drumncode/src/app/app.module.ts
+++ drumncode/src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatIconModule,
+    HttpClientModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
